96d71a8b5d65c2b4d49bb37fdfa11c0828d2c3b1
Kaycee_Easy <KayceeComett@gmail.com>
2026-06-28 00:30:08 +0100

    refactor: reorganize next.config.ts for improved readability and structure

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,25 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
 
-async rewrites() {
+const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  async rewrites() {
     return [
       {
         source: '/admin',
         destination: '/admin/index.html',
       },
     ];
-},
-
-reactCompiler: true,
+  },
 };
-module.exports = {
-//allowedDevOrigins: ['192.168.18.*'],
-allowedDevOrigins: ['10.198.134.*'],
-}
 
 export default nextConfig;
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
