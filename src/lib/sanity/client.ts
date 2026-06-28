fac62879e845c5a7bf82d4d5d66052085a72b737
Kaycee_Easy <KayceeComett@gmail.com>
2026-06-28 02:16:13 +0100

    fix: provide default value for projectId in sanity client configuration

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your_project_id',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-06-27',
   useCdn: true,
